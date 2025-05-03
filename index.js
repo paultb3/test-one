2879ccea006dee9042acc718f33e838e038b1f17
paultb3 <paulbenancio3000@gmail.com>
2025-05-03 17:27:08 -0500

    conceptos de grid

---
+++ index.js
@@ -0,0 +1,13 @@
+let bootn = document.getElementById('boton');
+
+bootn.addEventListener("click", function(){
+    alert('brennis es un gey');
+})
+
+bootn.addEventListener("mouseout",function(){
+    alert("fuera del elmeno")
+})
+
+bootn.addEventListener("mouseleave", function(){
+    alert("mause leave")
+})
